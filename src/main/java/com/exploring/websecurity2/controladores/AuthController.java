ea10ba1ae2c921e5c91b62695d4a6889d3e07063
Ernesto <vacevedo242@gmail.com>
2022-07-05 21:38:29 -0600

    prueba de push

--- src/main/java/com/exploring/websecurity2/controladores/AuthController.java
+++ src/main/java/com/exploring/websecurity2/controladores/AuthController.java
@@ -107,6 +107,6 @@ public class AuthController {
 		}
 		user.setRoles(roles);
 		userRepository.save(user);
-		return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+		return ResponseEntity.ok(new MessageResponse("User registered successfully!")); 
 	}
 }
